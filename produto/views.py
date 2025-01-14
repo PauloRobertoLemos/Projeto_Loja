663da268899e7ec7956cc0d2f68d63c8994331d6
Paulo Roberto Lemos <77172055+PauloRobertoLemos@users.noreply.github.com>
2025-01-14 14:10:44 -0300

    criação do feedback

--- produto/views.py
+++ produto/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .forms import ProdutoForm
 from django.shortcuts import redirect
 from .models import Produto
+from django.contrib import messages
+
 
 
 # Create views
@@ -12,7 +14,10 @@ def create_produto(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Produto criado com sucesso!')
             return redirect('list')
+        else:
+            messages.error(request, 'Erro ao criar o produto. Verifique os dados e tente novamente.')
     else:
         form = ProdutoForm()
     return render(request, 'create.html', {'form':form})
@@ -32,8 +37,10 @@ def update_produto(request, pk):
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Produto atualizado com sucesso!')
             return redirect('list')
-
+        else:
+            messages.error(request, 'Erro ao atualizar o produto. Verifique os dados e tente novamente.')
     else:
         form = ProdutoForm(instance=produto)
     return render(request, 'update.html', {'form':form})
@@ -44,4 +51,5 @@ def delete_produto(request, pk):
     produto = Produto.objects.get(id=pk)
     if request.method == 'POST':
         produto.delete()
+        messages.success(request, 'Produto excluído com sucesso!')
     return redirect('list')
