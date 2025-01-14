331b3cbc3adb33e72feddbbd4f78871168d621ed
Paulo Roberto Lemos <77172055+PauloRobertoLemos@users.noreply.github.com>
2025-01-14 12:14:55 -0300

    criação do formulário de criação e atualização das variaveis

--- produto/models.py
+++ produto/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Produto(models.Model):
     produto_id = models.CharField(max_length=20)
     produto_name = models.CharField(max_length=100)
-    produto_price = models.DecimalField(max_digits=10, decimal_places=2)
-    produto_qnd_est = models.PositiveIntegerField()
-    produto_dt_cri = models.DateTimeField(auto_now_add=True)
+    produto_preco = models.DecimalField(max_digits=10, decimal_places=2)
+    produto_quantidade_estoque = models.PositiveIntegerField()
+    produto_data_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.produto_name
