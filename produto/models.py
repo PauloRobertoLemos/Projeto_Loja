eb9949f4e86d3c0b9ada22e10a06bdddcedea64e
Paulo Roberto Lemos <77172055+PauloRobertoLemos@users.noreply.github.com>
2025-01-14 11:17:26 -0300

    Criação do CRUD

--- produto/models.py
+++ produto/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 class Produto(models.Model):
     produto_id = models.CharField(max_length=20)
-    
+    produto_name = models.CharField(max_length=100)
+    produto_price = models.DecimalField(max_digits=10, decimal_places=2)
+    produto_qnd_est = models.PositiveIntegerField()
+    produto_dt_cri = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.produto_name
--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-def Home(request):
-    return HttpResponse("Hello World")
+from .forms import ProdutoForm
+from django.shortcuts import redirect
+from .models import Produto
+
+
+# Create views
+def create_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = ProdutoForm()
+    return render(request, 'create.html', {'form':form})
+
+
+# list view
+def produto_list(request):
+    produto = Produto.objects.all()
+    return render(request, 'list.html', {'produtos':produto})
+
+
+# updates view
+def updates_produto(request, pk):
+    produto = Produto.objects.get(id=pk)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'update.html', {'form':form})
+
+# delete view
+
+def delete_produto(request, pk):
+    produto = Produto.objects.get(id=pk)
+    if request.method == 'POST':
+        produto.delete()
+    return redirect('list')
